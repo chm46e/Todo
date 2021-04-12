dedd2917dda665db978da70fa51064e6f471bbcf
Mr.Wolfy <kitarrrenee@gmail.com>
2021-04-12 20:45:26 +0300

    Added the inital todo.py file.

---
+++ todo.py
@@ -0,0 +1,367 @@
+import sys
+from termcolor import colored
+
+#Ideas;
+#  remove all green state notes
+
+#Global variables
+args = sys.argv
+args_len = len(args) - 1
+notefile = "/home/wolfy/.cache/todo/notes"
+groupfile = "/home/wolfy/.cache/todo/groups"
+alphabet = "abcdefghijklmonpqrstuvwxyz"
+
+#Minifunctions
+def readgroupfile():
+	file = open(groupfile, "r")
+	lines = file.readlines()
+	file.close()
+	return lines
+
+def readnotefile():
+	file = open(notefile, "r")
+	lines = file.readlines()
+	file.close()
+	return lines
+
+#Functions
+def list():
+	#todo l
+
+	group_id = 0 
+	color = "red" #The default color
+	ls_array = []
+	group_array = []
+
+	#Add group names to ls_array and group array
+	lines = readgroupfile()
+	for line in lines:
+		clean_line = line.replace("\n", "")
+		ls_array.append([clean_line, []])
+		group_array.append(clean_line)
+
+	#Get's the note count
+	line_count = len(readnotefile())
+
+	counter = 0
+	while counter != line_count:
+		#Split's the line by the space delimiter and put's them into variables
+		lines = readnotefile()
+		line = lines[counter].replace("\n", "")
+		note, state, group = line.split()
+
+		#Change the color depending on the state
+		if(state == "1"):
+			color = "red"
+		elif(state == "2"):
+			color = "yellow"
+		elif(state == "3"):
+			color = "green"
+
+		#Create the colored note
+		colored_note = colored(note, color)
+
+		#Tries to find a group match(group array and line group)
+		group_counter = 0
+		for gr_group in group_array:
+			if(gr_group == group):
+				group_id = group_counter
+				break
+			group_counter+=1
+
+		#Append's the note to the right group_array(dependent on the group_id)
+		ls_array[group_id][1].append(colored_note)
+
+		counter+=1
+
+	#Print's/Build's the lines
+	counter_alt = 0
+	for a in ls_array:
+		print(colored(alphabet[counter_alt] + ")", "green") + colored(a[0], "blue") + ":")
+		counter = 1
+		for b in a[1]:
+			print("      " + str(counter) + ". " + b)
+			counter+=1
+		counter_alt+=1
+
+
+def insert():
+	#todo i <group_char> <note>
+
+	#Find's the group letter position from alphabet
+	char_pos = alphabet.find(args[2])
+
+	#Open's and read's the groups
+	lines = readgroupfile()
+
+	counter = 0
+	for line in lines:
+		if(counter == char_pos):
+			group = line.replace("\n", "")
+
+			#Append's the note to the end of the note file
+			file=open(notefile, "a")
+			file.write(args[3] + " 1 " + group + "\n")
+			file.close()
+
+			break
+		counter+=1
+
+def delete():
+	#todo d <group_char + note_nr>
+
+	group_char = args[2][0]
+	note_nr = args[2].replace(args[2][0], "")
+
+	#Find's the group letter position from alphabet
+	char_pos = alphabet.find(group_char)
+
+	#Open's and read's the groups
+	group_lines = readgroupfile()
+
+	#Translates the group_char to a group name
+	group = group_lines[char_pos].replace("\n", "")
+
+	#Open's and read's the notes
+	note_lines = readnotefile()
+
+	#Open the note file in writing mode
+	file = open(notefile, "w")
+
+	counter = 0
+	line_counter = 0
+	for line in note_lines:
+		line_group = line.split()[2]
+
+		#Check's if group is correct
+		if(line_group == group):
+			counter+=1
+
+			#Check's if count is not correct
+			if(note_nr != str(counter)):
+				file.write(line)
+		else:
+			file.write(line)
+		line_counter+=1
+	file.close()
+
+def edit():
+	#todo e <group_char + note_nr> <note> <group>
+
+	group_char = args[2][0]
+	note_nr = args[2].replace(args[2][0], "")
+
+	#Find's the group letter position from alphabet
+	char_pos = alphabet.find(group_char)
+
+	#Open's and read's the groups
+	group_lines = readgroupfile()
+
+	#Translates the group_char to a group name
+	group = group_lines[char_pos].replace("\n", "")
+
+	#Open's and read's the notes
+	note_lines = readnotefile()
+
+	#Open the note file in writing mode
+	file = open(notefile, "w")
+
+	counter = 0
+	line_counter = 0
+	for line in note_lines:
+		line_split = line.split()
+		line_group = line_split[2]
+		line_state = line_split[1]
+
+		#Check's if group is correct
+		if(line_group == group):
+			counter+=1
+
+			#Check's if count is not correct
+			if(note_nr != str(counter)):
+				file.write(line)
+			else:
+				if(args_len == 4):
+					file.write(args[3] + " " + line_state + " " + args[4] + "\n")
+				else:
+					file.write(args[3] + " " + line_state + " " + group + "\n")
+		else:
+			file.write(line)
+		line_counter+=1
+	file.close()
+
+def makegroup():
+	file = open(groupfile, "a")
+	file.write(args[2] + "\n")
+	file.close()
+
+def removegroup():
+	group_exist = False
+	lines = readnotefile()
+	for line in lines:
+		if(line.split()[2].replace("\n", "") == args[2]):
+			group_exist = True
+			break
+	if(group_exist == True):
+		print("Cannot delete group:")
+		print("Group not empty.")
+		return 0
+
+	with open(groupfile,"r+") as f:
+	    new_f = f.readlines()
+	    f.seek(0)
+	    counter = 0
+	    for line in new_f:
+	        counter+=1
+	        if args[2] != line.replace("\n", ""):
+	        	f.write(line)
+	    f.truncate()
+
+	list()
+
+def changestate():
+	#todo s <group_char + note_nr> <state>
+
+	group_char = args[2][0]
+	note_nr = args[2].replace(args[2][0], "")
+
+	#Find's the group letter position from alphabet
+	char_pos = alphabet.find(group_char)
+
+	#Open's and read's the groups
+	group_lines = readgroupfile()
+
+	#Translates the group_char to a group name
+	group = group_lines[char_pos].replace("\n", "")
+
+	#Open's and read's the notes
+	note_lines = readnotefile()
+
+	#Open the note file in writing mode
+	file = open(notefile, "w")
+
+	counter = 0
+	line_counter = 0
+	for line in note_lines:
+		line_split = line.split()
+		line_group = line_split[2]
+		line_note = line_split[0]
+
+		#Check's if group is correct
+		if(line_group == group):
+			counter+=1
+
+			#Check's if count is correct
+			if(note_nr == str(counter)):
+				file.write(line_note + " " + args[3] + " " + group + "\n")
+			else:
+				file.write(line)
+		else:
+			file.write(line)
+		line_counter+=1
+	file.close()
+
+def removedone():
+	#todo rd
+
+	lines = readnotefile()
+
+	file = open(notefile, "w")
+
+	for line in lines:
+		if(line.split()[1] != "3"):
+			file.write(line)
+
+	file.close()
+
+if args_len == 0:
+	list()
+
+elif args[1] == "i" or args[1] == "insert":
+	#todo i <note> <group>
+	insert()
+	list()
+
+elif args[1] == "l" or args[1] == "list":
+	#todo l
+	list()
+
+elif args[1] == "d" or args[1] == "delete" or args[1] == "r" or args[1] == "remove":
+	#todo d <group> <note_nr>
+	delete()
+	list()
+
+elif args[1] == "e" or args[1] == "edit":
+	edit()
+	list()
+
+elif args[1] == "mg" or args[1] == "makegroup":
+	makegroup()
+	list()
+
+elif args[1] == "rg" or args[1] == "removegroup":
+	removegroup()
+
+elif args[1] == "s" or args[1] == "state":
+	changestate()
+	list()
+
+elif args[1] == "rd" or args[1] == "removedone":
+	removedone()
+	list()
+
+elif args[1] == "-h" or args[1] == "--help" or args[1] == "h" or args[1] == "help":
+	print("Simplistic todo terminal application created in Python3")
+	print("")
+	print("Setting an alias: 'alias todo='python3 /path/to/script.py' is highly recommended.")
+	print("If <cmd> is empty, the list command is executed.")
+	print("State: 1(red) -> Not started  2(yellow) -> Doing  3(green) -> Done")
+	print("")
+	print("Usage: todo <cmd> <arg1> ...")
+	print("Commands and args:")
+	print("  l or list -> Print all the notes")
+	print("    No parameters")
+	print("")
+	print("  i or insert -> Insert a note")
+	print("    <group_char> <note>")
+	print("")
+	print("  r or remove -> Remove a note")
+	print("    <group_char><note_nr>")
+	print("")
+	print("  e or edit -> Edit a note's text")
+	print("    <group_char><note_nr> <note>")
+	print("")
+	print("  mg or makegroup -> Make a group")
+	print("    <group_name>")
+	print("")
+	print("  rg or removegroup -> Remove a group")
+	print("    <group_name>")
+	print("")
+	print("  s or state -> Change the state of a note")
+	print("    <group_char><note_nr> <state>")
+	print("")
+	print("  rd or removedone -> Remove all done(green state) notes")
+	print("    No parameters")
+	print("")
+	print("Examples:")
+	print("Inserting a note 'Homework!' into the first group(School group):")
+	print("  todo i a Homework!")
+	print("")
+	print("Removing the eleventh note from the second group:")
+	print("  todo r b11")
+	print("")
+	print("List all the notes:")
+	print("  todo")
+	print("")
+	print("Edit the first note from second group to show 'Hello!':")
+	print("  todo e b1 Hello!")
+	print("")
+	print("Change the state of first group, third note to done(green):")
+	print("  todo s a3 3")
+	print("")
+	print("Made by: Mr.Wolfy")
+
+else:
+	print("Command not found")
+
+
